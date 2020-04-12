8ea5b2284744ec0efc9c1c0d5d8fd02dbe26239a
rsascha <sascha.rose@gmail.com>
2020-04-12 12:57:53 +0200

    Add TypeORM module configuration

--- my-test/src/app.module.ts
+++ my-test/src/app.module.ts
@@ -3,11 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
-import { UserHttpModule } from './user-http/user-http.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user';
 
 @Module({
-  imports: [UsersModule, UserHttpModule],
-  controllers: [AppController],
-  providers: [AppService, UsersService],
+    imports: [
+        UsersModule,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'password',
+            database: 'nestjs-db',
+            entities: [User],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService, UsersService],
 })
-export class AppModule {}
+export class AppModule { }
