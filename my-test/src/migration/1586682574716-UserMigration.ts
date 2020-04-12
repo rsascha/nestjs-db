72b2e4de317c258ce2fc7f599497aa44f4af2482
rsascha <sascha.rose@gmail.com>
2020-04-12 11:46:29 +0200

    First migration is working

--- my-test/src/migration/1586682574716-UserMigration.ts
+++ my-test/src/migration/1586682574716-UserMigration.ts
@@ -4,41 +4,29 @@ export class UserMigration1586682574716 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
     
-        await queryRunner.query(`
-            CREATE TABLE "users" (
-                "id" int(11) NOT NULL,
-                "first_name" varchar(20) DEFAULT NULL,
-                "last_name" varchar(20) DEFAULT NULL,
-                "age" smallint(6) NOT NULL
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-        `);
+        await queryRunner.query(
+            "CREATE TABLE `users` ("
+                + "`id` int(11) NOT NULL,"
+                + "`first_name` varchar(20) DEFAULT NULL,"
+                + "`last_name` varchar(20) DEFAULT NULL,"
+                + "`age` smallint(6) NOT NULL"
+            + ") ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+        );
     
-        await queryRunner.query(`
-            ALTER TABLE "users"
-            ADD PRIMARY KEY ("id");
-        `);
+        await queryRunner.query(
+            "ALTER TABLE `users` ADD PRIMARY KEY (`id`);"
+        );
 
-        await queryRunner.query(`
-            ALTER TABLE "users"
-            ADD PRIMARY KEY ("id");
-        `);
-
-        await queryRunner.query(`
-            ALTER TABLE "users"
-            MODIFY "id" int(11) NOT NULL AUTO_INCREMENT;
-        `);
-
-        await queryRunner.query(`
-            COMMIT;
-        `);
+        await queryRunner.query(
+            "ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;"
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
 
-        await queryRunner.query(`
-            DROP TABLE "users"
-        `);
-
+        await queryRunner.query(
+            "DROP TABLE `users`"
+        );
     }
 
 }
