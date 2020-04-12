eb7017fdbd73c873d0a1eb5d036732bc2ee9ecb0
rsascha <sascha.rose@gmail.com>
2020-04-12 11:32:16 +0200

    Add first migration

---
+++ my-test/src/migration/1586682574716-UserMigration.ts
@@ -0,0 +1,44 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UserMigration1586682574716 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    
+        await queryRunner.query(`
+            CREATE TABLE "users" (
+                "id" int(11) NOT NULL,
+                "first_name" varchar(20) DEFAULT NULL,
+                "last_name" varchar(20) DEFAULT NULL,
+                "age" smallint(6) NOT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        `);
+    
+        await queryRunner.query(`
+            ALTER TABLE "users"
+            ADD PRIMARY KEY ("id");
+        `);
+
+        await queryRunner.query(`
+            ALTER TABLE "users"
+            ADD PRIMARY KEY ("id");
+        `);
+
+        await queryRunner.query(`
+            ALTER TABLE "users"
+            MODIFY "id" int(11) NOT NULL AUTO_INCREMENT;
+        `);
+
+        await queryRunner.query(`
+            COMMIT;
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+
+        await queryRunner.query(`
+            DROP TABLE "users"
+        `);
+
+    }
+
+}
