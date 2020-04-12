39a39196af09ebf4ab8f6187695874ba44e43194
rsascha <sascha.rose@gmail.com>
2020-04-12 11:07:03 +0200

    Perform fixed after "typeorm init"

--- my-test/src/users/users.module.ts
+++ my-test/src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../entities/user';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from '../entity/user';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User])],
--- my-test/src/users/users.service.ts
+++ my-test/src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
+import { User } from '../entity/user';
 
 @Injectable()
 export class UsersService {
